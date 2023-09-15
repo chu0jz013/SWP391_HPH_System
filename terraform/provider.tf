5bae1a0e4a64254c50409e6fca44da4e30941511
haikn <williamkieu2003@gmail.com>
2023-09-15 20:03:53 +0700

    init

---
+++ terraform/provider.tf
@@ -0,0 +1,26 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.74.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
+
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
